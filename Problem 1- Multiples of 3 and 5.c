755518a2b72170b2c01303129da5e7820c2bd867
pytxt <63718466+pytxt@users.noreply.github.com>
2021-01-17 11:19:01 +0530

    Update Problem 1- Multiples of 3 and 5.c

--- Problem 1- Multiples of 3 and 5.c
+++ Problem 1- Multiples of 3 and 5.c
@@ -2,3 +2,18 @@
 Q1. If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
+
+#include<stdio.h>
+
+int main(void) {
+  int lim=0, sum=0;
+    for(lim=0;lim<1000;lim++)
+    {
+        if((lim%3==0)||(lim%5==0))  // finding multiples of 3 or 5
+        {
+            sum=sum+lim;
+        }
+    }
+printf("Sum of Multiples of 3 or 5 = %d",sum);  // printing final result
+return 0;
+}
